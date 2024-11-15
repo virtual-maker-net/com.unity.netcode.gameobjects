5d97c457fa298efadc6f5ef0b54f15f5470736f9
Noel Stephens <noel.stephens@unity3d.com>
2024-11-14 18:18:34 -0600

    fix: in-sceneobject networkobject update in editor tool update (up-port-fix) (#3123)
    * fix
    
    Assign the current NetworkObject prefab to be updated.
    
    * style
    
    adding comment

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkObject.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkObject.cs
@@ -106,6 +106,8 @@ namespace Unity.Netcode
         internal void RefreshAllPrefabInstances()
         {
             var instanceGlobalId = GlobalObjectId.GetGlobalObjectIdSlow(this);
+            // Assign the currently selected instance to be updated
+            NetworkObjectRefreshTool.PrefabNetworkObject = this;
             if (!PrefabUtility.IsPartOfAnyPrefab(this) || instanceGlobalId.identifierType != k_ImportedAssetObjectType)
             {
                 EditorUtility.DisplayDialog("Network Prefab Assets Only", "This action can only be performed on a network prefab asset.", "Ok");
